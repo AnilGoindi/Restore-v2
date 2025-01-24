9deb8e99aa7e88a335d07dbdacb352eecbb4b173
AnilGoindi <114911637+AnilGoindi@users.noreply.github.com>
2025-01-24 16:43:36 +0000

    Step 42, NavBar and Routing

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,11 +1,9 @@
-import { useEffect, useState } from "react"
-import { Product } from "../models/product";
-import Catalog from "../../features/calalog/Catalog";
+import { useState } from "react"
 import { Box, Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import NavBar from "./NavBar";
+import { Outlet } from "react-router-dom";
 
-function App() {
-  const [products, setProducts] = useState<Product[]>([]);
+function App() {  
   const [darkMode, setDarkMode] = useState(false);
   const paletteType = darkMode ? 'dark': 'light';
 
@@ -22,13 +20,6 @@ function App() {
     setDarkMode(!darkMode);
   }
 
-  useEffect(() => {
-    fetch('https://localhost:5001/api/products')
-      .then(response => response.json())
-      .then(data => setProducts(data))
-  }, [])
-
-
   return (
 
     <ThemeProvider theme={theme}>
@@ -44,7 +35,7 @@ function App() {
         }}
       >
         <Container maxWidth='xl' sx={{mt: 8}}>      
-          <Catalog products={products} />      
+          <Outlet />      
         </Container>
       </Box>
       
